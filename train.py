a29cd48445a7d1830ad07ecffea3456217cf95d2
MartVASS <vasseurmartin2003@gmail.com>
2026-04-08 22:27:42 +0200

    Structure the train file

--- train.py
+++ train.py
@@ -1,13 +1,27 @@
+import torch
+
 from utils.util_functions import *
 
-# 1. Get data ready
+def main():
+    # 1. Get data ready
 
-check_and_download_data()
-adjust_data()  # To allow using Image Folder on data
+    check_and_download_data()
+    adjust_data()  # To allow using Image Folder on data
 
-# 2. Split data into training and test sets
+    # 2. Split data into training and test sets
 
-train_dataset, test_dataset = split_dataset()
+    train_dataset, test_dataset = split_dataset()
 
-print(f"Length of train dataset: {len(train_dataset)}")
-print(f"Length of test dataset: {len(test_dataset)}")
+    print(f"Length of train dataset: {len(train_dataset)}")
+    print(f"Length of test dataset: {len(test_dataset)}")
+
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers = 4, persistent_workers = True)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=32, shuffle=False, num_workers = 4, persistent_workers = True)
+
+    # Check out what's inside the training dataloader
+    train_features_batch, train_labels_batch = next(iter(train_loader))
+    print(train_features_batch.shape) 
+    print(train_labels_batch.shape)
+
+if __name__ == '__main__':
+    main()
