99ec5afd9c0acfbab1dc79a2c8207bdacb6a8422
MartVASS <vasseurmartin2003@gmail.com>
2026-04-08 21:42:01 +0200

    English prints and new function to adjust data from dataset

--- utils/util_functions.py
+++ utils/util_functions.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import zipfile
 import numpy as np
 import urllib.request
@@ -21,24 +22,37 @@ def check_and_download_data():
     url = "http://cs231n.stanford.edu/tiny-imagenet-200.zip"
 
     if not os.path.exists(dataset_path):
-        print("Dataset non trouvé. Téléchargement en cours...")
+        print("Dataset not found. Downloading in progress...")
         os.makedirs("dataset", exist_ok=True)
         
-        print(f"📡 Téléchargement depuis {url}...")
+        print(f"📡 Downloading from {url}...")
         # Cette ligne télécharge le fichier physiquement
         urllib.request.urlretrieve(url, zip_path)
 
-        print("📦 Extraction des fichiers (cela peut prendre 2-3 minutes)...")
+        print("📦 Extracting files (this operation can take 2-3 minutes)...")
         with zipfile.ZipFile(zip_path, 'r') as zip_ref:
             zip_ref.extractall(dataset_path)
         
-        print("🧹 Nettoyage du fichier ZIP...")
+        print("🧹 Cleaning ZIP file...")
         os.remove(zip_path)
-        print("✅ Terminé ! Le dataset est prêt dans :", dataset_path)
+        print("✅ Finish, you can find the dataset at:", dataset_path)
     else:
         
         # On vérifie si le dossier ET un fichier clé existent
         if os.path.exists(dataset_path) and os.path.exists(os.path.join(dataset_path, "words.txt")):
-            print("✅ Dataset déjà extrait et complet.")
+            print("✅ Dataset already extracted and complete.")
 
 
+def adjust_data():
+
+
+    with open('dataset/tiny-imagenet-200/tiny-imagenet-200/val/val_annotations.txt') as f:
+        
+        for line in f:
+            fn, cls, *_ = line.split('\t')
+            os.makedirs(f'tiny-imagenet/tiny-imagenet-200/val/{cls}', exist_ok=True)
+
+            shutil.copyfile(f'tiny-imagenet/tiny-imagenet-200/val/images/{fn}', f'tiny-imagenet/tiny-imagenet-200/val/{cls}/{fn}')
+
+    shutil.rmtree('tiny-imagenet/tiny-imagenet-200/val/images')
+    print("succesfully adjusting data")
